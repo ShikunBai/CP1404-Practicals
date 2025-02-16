297cee74d7de97a4725ac7dfdf9ec62c0c0290e1
kysen <shikun.bai@my.jcu.edu.au>
2025-02-16 17:19:43 +0800

    Random Numbers

---
+++ prac_03/randoms.py
@@ -0,0 +1,10 @@
+
+# 1. random integers, 5, 20
+# 2. random odd numbers, 3, 9. Can not produce 4
+# 3. random floating numbers, 2.5, 5.5
+
+import random
+
+print(random.uniform(1, 100))
+print(random.randint(1, 100))
+
