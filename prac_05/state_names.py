621c8ae6301dedf71200deb93dbb8dc9b7ae3eba
kysen <shikun.bai@my.jcu.edu.au>
2025-03-02 18:35:39 +0800

    Walkthrough Example

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,15 +4,28 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
+
+CODE_TO_NAME = {
+    "QLD": "Queensland",
+    "NSW": "New South Wales",
+    "NT": "Northern Territory",
+    "WA": "Western Australia",
+    "ACT": "Australian Capital Territory",
+    "VIC": "Victoria",
+    "TAS": "Tasmania",
+    "SA": "South Australia"
+}
+CODE_LENGTH = 3
+
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
-    state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
+
+try:
+    print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+except KeyError:
+    print("Invalid short state")
+state_code = input("Enter short state: ").upper()
+
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:<{CODE_LENGTH}} is {name}")
